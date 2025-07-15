d40744ef81e9089e62427a3b9e0e678586b326f2
only-dsa <itsjannesar@gmail.com>
2025-07-15 22:19:13 +0530

    Function Lecture

---
+++ BITS_BCA_3rd/src/Lecture9/FunctionPractice.java
@@ -0,0 +1,28 @@
+package Package9;
+
+import java.util.HashMap;
+
+public class FunctionPractice {
+	
+//	public static void checkEven() {
+//		
+//	}
+	
+	public static void main(String[] vikas) {
+		String s;
+		System.out.println("Hello");
+		// can we create our method here? ---> we can't create our method
+	}
+	
+//	public static void checkEven() {
+//		
+//	}
+	
+	   checkEven() {
+		
+	}
+
+}
+
+
+// we can't create our method
